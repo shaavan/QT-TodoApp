0d673f8cdb163e13c731f64a240bb876ad00778e
Shashwat <svangani239@gmail.com>
2021-09-23 00:38:24 +0530

    1.1: Signals and Slots
    At this stage, there is a text Label and a QPushButton
    When the button is clicked the text: "User Clicked on the Button"
    will be displayed in the debug console. The connection when
    the button is clicked will modified to add a new task
    in the todolist in later commits.

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,15 +1,24 @@
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(ui->addTaskButton, &QPushButton::clicked,
+            this, &MainWindow::addTask);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::addTask()
+{
+    qDebug()<<"User Clicked on the Button";
 }
 
