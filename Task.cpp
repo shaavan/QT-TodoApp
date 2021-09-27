382675bd9410a5c328769ef31755d2d8f6a27321
Shashwat <svangani239@gmail.com>
2021-09-28 00:10:56 +0530

    1.5: Distributing Code Responsibility
    - Added a new rename() function that will allow editing the name of an
    already added task
    - For this purpose the QInputDialog class is used again
    - The function is declared under the Task form itself making it
    completely autonomous and free from MainWindow form.

--- Task.cpp
+++ Task.cpp
@@ -1,17 +1,34 @@
 #include "Task.h"
 #include "ui_Task.h"
 
+#include <QInputDialog>
+
 Task::Task(const QString& name, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Task)
 {
     ui->setupUi(this);
     setName(name);
+    connect(ui->editButton, &QPushButton::clicked, this, &Task::rename);
 }
 
 Task::~Task()
 {
     delete ui;
+}
+
+void Task::rename()
+{
+    bool ok;
+    QString value = QInputDialog::getText(this,
+                                         tr("Edit task"),
+                                         tr("Task name"),
+                                         QLineEdit::Normal,
+                                         this->name(), &ok);
+    if(ok && !value.isEmpty()) {
+        setName(value);
+    }
+
 }
 
 void Task::setName(const QString& name)  //Setter: Sets Task's name
